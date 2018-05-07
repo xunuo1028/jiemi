960effef11a08b3aa51cbd5a03bc1aba696acb5f
xunuo <xunuo@mail.jj.cn>
2018-05-07 15:01:24 +0800

    update

--- Assets/Lua/Logic/PanelNum.lua
+++
@@ -1,6 +0,0 @@
-require "System.Global"
-
-class("PanelNum")
-
-PanelNum.pn = {}
-PanelNum.pn.waiting = 999
--- Assets/StreamingAssets/lua/Logic/MsgPool.lua
+++
@@ -1,8 +0,0 @@
-require "System.Global"
-
-class("MsgPool")
-
-MsgPool.Msg = {
-		MSG_ChangUIUserName = 0x0001,
-		MSG_TestUI = 0x1001,
-	}
